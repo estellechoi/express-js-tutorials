cce81ad41b3b99597111d5d75dfe72f033961c42
Estelle Choi <estele.choi@gmail.com>
2020-06-14 17:38:25 +0900

    app config and middle wares, routers

--- first-app/main.js
+++ first-app/main.js
@@ -57,6 +57,12 @@ app.use((req, res, next) => {
 	res.status(404).send("Sorry cant find that!");
 });
 
+// Error Handler is called when next(err) is called
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send("Something broke!");
+});
+
 app.listen(port, () =>
 	console.log(`Example app listening at http://localhost:${port}`)
 );
