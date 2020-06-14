e5bcdd733fcc2b322345eb938d08a66ab8eff410
Estelle Choi <estele.choi@gmail.com>
2020-06-14 17:27:59 +0900

    express app

--- first-app/main.js
+++ first-app/main.js
@@ -7,6 +7,9 @@ const bodyParser = require("body-parser"); // request body parser
 const compression = require("compression"); // response get compressed for cutting network cost
 const requestTime = require("./src/requestTime");
 
+// middle ware by express
+const static = express.static("img");
+
 // app's top-level generic use of middle wares
 app.use(compression({ filter: shouldCompress }));
 const urlencodedParser = bodyParser.urlencoded({ extended: false }); // parse application/x-www-form-urlencoded
@@ -14,6 +17,7 @@ const jsonParser = bodyParser.json(); // parse application/json
 app.use(urlencodedParser);
 app.use(jsonParser);
 app.use(requestTime());
+app.use("/public", static); // virtual path prefix
 
 // services
 var topic = require("./src/topic");
@@ -49,35 +53,10 @@ app.get("/delete_author_process", (req, res) =>
 	res.redirect(302, user.deleteProcess(req, res))
 );
 
+app.use((req, res, next) => {
+	res.status(404).send("Sorry cant find that!");
+});
+
 app.listen(port, () =>
 	console.log(`Example app listening at http://localhost:${port}`)
 );
-
-// 	if (pathname === "/") {
-// 		if (queryData.id === undefined) topic.home(request, response, queryData);
-// 		else topic.page(request, response, queryData);
-// 	} else if (pathname === "/create") {
-// 		topic.create(request, response, queryData);
-// 	} else if (pathname === "/create_process") {
-// 		topic.createProcess(request, response, queryData);
-// 	} else if (pathname === "/update") {
-// 		topic.update(request, response, queryData);
-// 	} else if (pathname === "/update_process") {
-// 		topic.updateProcess(request, response, queryData);
-// 	} else if (pathname === "/delete_process") {
-// 		topic.deleteProcess(request, response, queryData);
-// 	} else if (pathname === "/author") {
-// 		user.home(request, response, queryData);
-// 	} else if (pathname === "/create_author_process") {
-// 		user.createProcess(request, response, queryData);
-// 	} else if (pathname === "/update_author") {
-// 		user.update(request, response, queryData);
-// 	} else if (pathname === "/update_author_process") {
-// 		user.updateProcess(request, response, queryData);
-// 	} else if (pathname === "/delete_author_process") {
-// 		user.deleteProcess(request, response, queryData);
-// 	} else {
-// 		response.writeHead(404);
-// 		response.end("Not found");
-// 	}
-// });
