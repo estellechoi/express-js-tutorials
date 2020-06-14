2d195804fae5ecd59acf64311f999973d05881b5
Estelle Choi <estele.choi@gmail.com>
2020-06-14 20:59:10 +0900

    routers and middle wares

--- first-app/main.js
+++ first-app/main.js
@@ -5,19 +5,24 @@ const port = 3000;
 
 const bodyParser = require("body-parser"); // request body parser
 const compression = require("compression"); // response get compressed for cutting network cost
-const requestTime = require("./src/requestTime");
 
 // middle ware by express
 const static = express.static("img");
 
 // app's top-level generic use of middle wares
-app.use(compression({ filter: shouldCompress }));
+app.use(compression());
 const urlencodedParser = bodyParser.urlencoded({ extended: false }); // parse application/x-www-form-urlencoded
 const jsonParser = bodyParser.json(); // parse application/json
 app.use(urlencodedParser);
 app.use(jsonParser);
-app.use(requestTime());
 app.use("/public", static); // virtual path prefix
+
+// self-made middle ware
+app.use((req, res, next) => {
+	req.requestTime = Date.now();
+	next(); // call next middle ware
+	// next('route'); call next router's middle ware
+});
 
 // services
 var topic = require("./src/topic");
@@ -30,29 +35,30 @@ app.get("/:id", (req, res) => {
 	console.log(req.params);
 	res.send(topic.page(req, res));
 });
-app.get("/create", (req, res) => res.send(topic.create(req, res)));
-app.post("/create_process", (req, res) =>
+app.get("/topic/create", (req, res) => res.send(topic.create(req, res)));
+app.post("/topic/create_process", (req, res) =>
 	res.redirect(302, topic.createProcess(req, res))
 );
-app.get("/update", (req, res) => res.send(topic.update(req, res)));
-app.post("/update_process", (req, res) =>
+app.get("/topic/update", (req, res) => res.send(topic.update(req, res)));
+app.post("/topic/update_process", (req, res) =>
 	res.redirect(topic.updateProcess(req, res))
 );
-app.post("/delete_process", (req, res) =>
+app.post("/topic/delete_process", (req, res) =>
 	res.redirect(topic.deleteProcess(req, res))
 );
 app.get("/author", (req, res) => res.send(user.home(req, res)));
-app.post("/create_author_process", (req, res) =>
+app.post("/author/create_process", (req, res) =>
 	res.redirect(user.createProcess(req, res))
 );
-app.get("/update_author", (req, res) => res.send(user.update(req, res)));
-app.post("/update_author_process", (req, res) =>
+app.get("/author/update", (req, res) => res.send(user.update(req, res)));
+app.post("/author/update_process", (req, res) =>
 	res.redirect(302, user.updateProcess(req, res))
 );
-app.get("/delete_author_process", (req, res) =>
+app.get("/author/delete_process", (req, res) =>
 	res.redirect(302, user.deleteProcess(req, res))
 );
 
+// 404 eror
 app.use((req, res, next) => {
 	res.status(404).send("Sorry cant find that!");
 });
