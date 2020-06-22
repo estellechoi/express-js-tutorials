d56f5201a47f3df2bc5646765427dd4bd4569991
Estelle Choi <estele.choi@gmail.com>
2020-06-22 16:06:49 +0900

    routers for topic prefixed requests

--- first-app/router/topic.js
+++ first-app/router/topic.js
@@ -5,22 +5,12 @@ const topic = require("./../src/topic");
 
 // routing
 // pretty url without queryString, which contributes to SEO.
-router.get("/", (req, res) => res.send(topic.home(req, res)));
-router.get("/:id", (req, res) => {
-	console.log(req.params);
-	res.send(topic.page(req, res));
-});
-
-router.get("/create", (req, res) => res.send(topic.create(req, res)));
-router.post("/create_process", (req, res) =>
-	res.redirect(302, topic.createProcess(req, res))
-);
-router.get("/update", (req, res) => res.send(topic.update(req, res)));
-router.post("/update_process", (req, res) =>
-	res.redirect(topic.updateProcess(req, res))
-);
-router.post("/delete_process", (req, res) =>
-	res.redirect(topic.deleteProcess(req, res))
-);
+router.get("/", (req, res) => topic.home(req, res));
+router.get("/:id", (req, res) => topic.page(req, res));
+router.get("/create", (req, res) => topic.create(req, res));
+router.post("/create_process", (req, res) => topic.createProcess(req, res));
+router.get("/update", (req, res) => topic.update(req, res));
+router.post("/update_process", (req, res) => topic.updateProcess(req, res));
+router.post("/delete_process", (req, res) => topic.deleteProcess(req, res));
 
 module.exports = router;
