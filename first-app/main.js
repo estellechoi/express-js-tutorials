9a9e5b81cf166029381ba92456745625d3ecdba1
Estelle Choi <estele.choi@gmail.com>
2020-06-23 17:08:18 +0900

    routers, error handler, listener, middlewares

--- first-app/main.js
+++ first-app/main.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const connection = require("./src/db");
 
 const bodyParser = require("body-parser"); // request body parser
 const compression = require("compression"); // response get compressed for cutting network cost
@@ -58,80 +57,12 @@ app.use(flash());
 // 	// res.render("index", { messages: req.flash("info") });
 // });
 
-// passport.js code should be after session code.
-const passport = require("passport"),
-	LocalStrategy = require("passport-local").Strategy;
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// storing session when a user signs in successfully.
-passport.serializeUser((user, done) => {
-	// 'user' object, the first arg, is the object from database when authentication success.
-	done(null, user.id);
-	// then, the seconde arg of done() becomes the value for 'user' property of the 'passport' object  in req.session so it is saved in session store.
-});
-
-// called whenever user accesses after signin
-passport.deserializeUser((id, done) => {
-	console.log(id);
-	connection.query(`SELECT * FROM user WHERE id = ?`, [id], (err, result) => {
-		console.log(result[0]);
-		done(err, result[0]);
-		// the second arg object of done() becomes the value for 'user' property of request object in routers' callback.
-	});
-});
-
-// passport configuration
-passport.use(
-	new LocalStrategy(
-		{
-			usernameField: "email",
-			passwordField: "password",
-		},
-		(email, password, done) => {
-			connection.query(
-				`SELECT * FROM user WHERE email = ?`,
-				[email],
-				(err1, emailResult) => {
-					// console.log(err1, emailResult);
-					if (err1) return done(err1);
-					if (!emailResult.length)
-						return done(null, false, { message: "Incorrect email." });
-
-					connection.query(
-						`SELECT * FROM user WHERE email = ? and password = ?`,
-						[email, password],
-						(err2, result) => {
-							if (err2) return done(err2);
-							if (!result.length)
-								return done(null, false, { message: "Incorrect password." });
-							return done(null, result[0]);
-						}
-					);
-				}
-			);
-		}
-	)
-);
+const passport = require("./lib/passport")(app);
 
 app.use("/", indexRouter);
 app.use("/topic", topicRouter);
 app.use("/author", authorRouter);
-app.use("/auth", authRouter);
-
-// passport authentication
-app.post(
-	"/auth/signin_process",
-	passport.authenticate("local", {
-		successRedirect: "/",
-		failureRedirect: "/auth/signin",
-		// this option insert flash : {error: ['message']} for data field into session table, calling req.flash() method inside.
-		failureFlash: true,
-		// flash: {success: ['message']}
-		successFlash: false,
-	})
-);
+app.use("/auth", authRouter(passport));
 
 // 404 eror
 app.use((req, res, next) => {
