167a6542503247f8f1ac2e34d0b5d62f719e2420
Estelle Choi <estele.choi@gmail.com>
2020-06-23 12:37:30 +0900

    express.Router()

---
+++ first-app/router/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const auth = require("./../src/auth");
+
+// routing
+router.get("/signin", (req, res) => auth.signin(req, res));
+router.post("/signin_process", (req, res) => auth.signinProcess(req, res));
+
+router.get("/signup", (req, res) => auth.signup(req, res));
+router.post("/signup_process", (req, res) => auth.signupProcess(req, res));
+router.get("/signout_process", (req, res) => auth.signoutProcess(req, res));
+
+module.exports = router;
