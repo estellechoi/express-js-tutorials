7f4493bae8e011320ff27a4d417ae43c8caae247
Estelle Choi <estele.choi@gmail.com>
2020-06-23 16:28:03 +0900

    express.Router()

--- first-app/router/auth.js
+++ first-app/router/auth.js
@@ -5,7 +5,7 @@ const auth = require("./../src/auth");
 
 // routing
 router.get("/signin", (req, res) => auth.signin(req, res));
-router.post("/signin_process", (req, res) => auth.signinProcess(req, res));
+// router.post("/signin_process", (req, res) => auth.signinProcess(req, res));
 
 router.get("/signup", (req, res) => auth.signup(req, res));
 router.post("/signup_process", (req, res) => auth.signupProcess(req, res));
--- first-app/router/author.js
+++ first-app/router/author.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const user = require("./../src/user");
+const author = require("../src/author");
 
-router.get("", (req, res) => res.send(user.home(req, res)));
+router.get("", (req, res) => res.send(author.home(req, res)));
 router.post("/create_process", (req, res) =>
-	res.redirect(user.createProcess(req, res))
+	res.redirect(author.createProcess(req, res))
 );
-router.get("/update", (req, res) => res.send(user.update(req, res)));
+router.get("/update", (req, res) => res.send(author.update(req, res)));
 router.post("/update_process", (req, res) =>
-	res.redirect(302, user.updateProcess(req, res))
+	res.redirect(302, author.updateProcess(req, res))
 );
 router.get("/delete_process", (req, res) =>
-	res.redirect(302, user.deleteProcess(req, res))
+	res.redirect(302, author.deleteProcess(req, res))
 );
 
 module.exports = router;
